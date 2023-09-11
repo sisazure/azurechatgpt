15eb42e606e9bf76fa4f695029abaf8a1bd316dd
sisazure <95276496+sisazure@users.noreply.github.com>
2023-09-11 17:21:08 +0900

    Update menu.tsx

--- features/menu/menu.tsx
+++ features/menu/menu.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { BarChartHorizontalBig } from "lucide-react";
+//import { BarChartHorizontalBig } from "lucide-react";
 import Link from "next/link";
 import { UserProfile } from "../user-profile";
 
@@ -16,13 +16,13 @@ export const MainMenu = () => {
             <AvatarImage src="/ai-icon.png" />
           </Avatar>
         </Link>
-        <Link
-          href="/reporting"
-          className="w-10 h-10 p-2 items-center justify-center flex border rounded-full"
-          title="Reporting"
-        >
-          <BarChartHorizontalBig />
-        </Link>
+//        <Link
+//          href="/reporting"
+//          className="w-10 h-10 p-2 items-center justify-center flex border rounded-full"
+//          title="Reporting"
+//        >
+//          <BarChartHorizontalBig />
+//        </Link>
       </div>
       <UserProfile />
     </div>
