d09a05918b741e8f68547e2f5b7ac42e7abfa451
sisazure <95276496+sisazure@users.noreply.github.com>
2023-09-11 18:33:34 +0900

    Update menu.tsx

--- features/menu/menu.tsx
+++ features/menu/menu.tsx
@@ -18,7 +18,7 @@ export const MainMenu = () => {
         </Link>
         <Link
           href="/reporting"
-          className="w-0 h-0 p-2 items-center justify-center flex border rounded-full"
+          className="w-0 h-0 p-0 items-center justify-center flex border rounded-full"
           title="Reporting"
         >
           <BarChartHorizontalBig size={0}/>
